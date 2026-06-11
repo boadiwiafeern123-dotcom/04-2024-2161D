315c75ae3e6bf618872ca3b23c6db32ddba1f793
boadiwiafeern123-dotcom <boadiwiafeern123@gmail.com>
2026-06-11 00:58:10 +0000

    Add files via upload

---
+++ javawork2.java
@@ -0,0 +1,18 @@
+public class javawork2 {
+    public static void main(String[] args) {
+    System.out.println(
+            "x                      x");
+    System.out.println(
+            "x                      x");
+    System.out.println(
+            "x       xxxxxxxxxx      x");
+    System.out.println(
+            "xxxxx   x        x  xxxxx");
+    System.out.println(
+            "x   x   x        x   x  x");
+    System.out.println(
+            "x   x   x        x   x  x");
+
+}
+}
+
